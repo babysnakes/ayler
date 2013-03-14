97c39761313259142bd1e5aeb309709e1f40e670
Haim Ashkenazi <haim@babysnakes.org>
2013-03-14 19:37:40 +0200

    Updated dependencies.
    It's been 9 since I last edited this project so I updated all required
    gems and twitter bootstrap.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.nrepl "0.2.0-RC1"]
-                 [ring/ring-core "1.1.6"]
-                 [ring/ring-jetty-adapter "1.1.6"]
-                 [enlive "1.0.1" :exclusions [org.clojure/clojure]]
-                 [compojure "1.1.3"
+                 [org.clojure/tools.nrepl "0.2.2"]
+                 [ring/ring-core "1.1.8"]
+                 [ring/ring-jetty-adapter "1.1.8"]
+                 [enlive "1.1.1"]
+                 [compojure "1.1.5"
                   :exclusions [org.clojure/tools.macro]]
-                 [com.taoensso/timbre "1.2.0"]
+                 [com.taoensso/timbre "1.5.2"]
                  [org.clojure/tools.cli "0.2.2"]]
 
   :profiles {:dev
