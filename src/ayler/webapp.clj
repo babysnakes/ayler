d017d2c8c1658f81aec9d807f837e8fc4b9a4da6
Haim Ashkenazi <haim@babysnakes.org>
2013-03-22 11:26:53 +0200

    Main functionality exposed via api.
    All the required functionality for the first stage is now exposed via
    the API.

--- src/ayler/webapp.clj
+++ src/ayler/webapp.clj
@@ -2,8 +2,9 @@
   "The webapp"
   (:require [ring.util.response :refer (resource-response)]
             [compojure.route :as route]
-            [compojure.handler :as handler])
-  (:use [compojure.core :only (defroutes GET)]
+            [compojure.handler :as handler]
+            [ayler.api :as api])
+  (:use [compojure.core :only (defroutes GET context)]
         [ayler.helpers :only (var-route)]))
 
 (defn- render
@@ -13,6 +14,7 @@
 
 (defroutes app-routes
   (GET "/" _ (resource-response "/public/index.html"))
+  (context "/api" [] (var-route api/app))
   (route/resources "/")
   (route/not-found "NOT FOUND"))
 
---
+++ test/ayler/api_test.clj
@@ -0,0 +1,19 @@
+(ns ayler.api-test
+  (:require [ayler.api :as api])
+  (:use clojure.test))
+
+(deftest converting-to-name-and-url
+  (testing "when input is nil"
+    (is (= (#'api/convert-to-name-and-url nil) '())))
+  (testing "escaping urls"
+    (let [result (#'api/convert-to-name-and-url '(hello world!))]
+      (is (= (:url (second result)) "world%21")))))
+
+(deftest only-applying-parser-to-successfull-results
+  (testing "un-successfull result"
+    (let [data {:status :error :response "data"}]
+      (is (= (#'api/apply-handler-if-successfull data #(%)) data))))
+  (testing "successfull result"
+    (is (= (#'api/apply-handler-if-successfull {:status :done :response [4 5]}
+                                               (fn [x] (map inc x)))
+           {:status :done :response [5 6]}))))
