7effd9eb42d1b5a757c4ccdf5237a09e499d0118
Haim Ashkenazi <haim@babysnakes.org>
2013-04-08 10:55:47 +0300

    Added unit-tests to app.js.
    The amount of boilerplate required for these testing is terrible. Both
    the code and the tests are ugly.

--- test/javascript/unit/allSpecs.js
+++ test/javascript/unit/allSpecs.js
@@ -1,7 +1,7 @@
 describe("Application: Ayler", function() {
-  describe("Controller: MainCtrl", function() {
-    var ctrl, scope;
+  var ctrl, scope;
 
+  describe("Controller: MainCtrl", function() {
     beforeEach(function() {
       scope = {};
       ctrl = new MainCtrl(scope);
@@ -43,7 +43,7 @@ describe("Application: Ayler", function() {
         scope.errorHandler("hello world", 403);
         expect(scope.errors[0]).toBe("Your session has expired. Please refresh the browser.");
       });
-      
+
       it("sets anyErrors on every error", function() {
         scope.errorHandler("hello world");
         expect(scope.anyErrors).toBeTruthy();
@@ -60,11 +60,158 @@ describe("Application: Ayler", function() {
         scope.clearErrors();
         expect(scope.anyErrors).toBeFalsy();
       });
-      
+
       it("clears the errors", function() {
         scope.clearErrors();
         expect(scope.errors.length).toBe(0);
       });
+    });
+  });
+
+  describe("Controller: NamespaceListCtrl", function() {
+    var $httpBackend, location;
+    var nsResponse = {
+      "status": "done",
+      "response": [{"name": "clojure.core", "url": "clojure.core"},
+                   {"name": "clojure.main", "url": "clojure.main"},
+                   {"name": "other.ns", "url": "other.ns"}]};
+    var varResponse = {
+      "status": "done",
+      "response": [{"name":"*","url":"clojure.core/%2A"},
+                   {"name":"*'","url":"clojure.core/%2A%27"},
+                   {"name":"*1","url":"clojure.core/%2A1"}]};
+
+    beforeEach(inject(function($location, $rootScope, $controller, _$httpBackend_) {
+      scope = $rootScope.$new();
+      scope.handleResponse = function() {};
+      $httpBackend = _$httpBackend_;
+      $httpBackend.when('GET', "/api/ls").respond(200, nsResponse);
+      ctrl = $controller(NamespaceListCtrl, {$scope: scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("#namespacesHandler assigns namespaces", function() {
+      scope.namespacesHandler(nsResponse["response"]);
+      expect(scope.namespaces[0]["name"]).toBe("clojure.core");
+    });
+
+    it("#varsHandler assigns vars", function() {
+      scope.varsHandler(varResponse["response"]);
+      expect(scope.vars[2]["name"]).toBe("*1");
+    });
+
+    it("#resetVarsFilter should do as advertised", function() {
+      scope.vrs = "filter";
+      scope.resetVarsFilter();
+      expect(scope.vrs).toBe("");
+    });
+  });
+
+  describe("Controller: NamespaceCtrl", function() {
+    var $httpBackend, location;
+
+    beforeEach(inject(function($routeParams, $rootScope, $controller, _$httpBackend_) {
+      scope = $rootScope.$new();
+      $routeParams.namespace = "clojure.core";
+      scope.handleResponse = function() {};
+      scope.loadVars = function() {};
+      scope.resetVarsFilter = function() {};
+      scope.setTitle = function() {};
+      spyOn(scope, "resetVarsFilter");
+      spyOn(scope, "setTitle");
+      $httpBackend = _$httpBackend_;
+      $httpBackend.when('GET', "/api/ls").respond(200, '');
+      $httpBackend.when('GET', "/api/doc/clojure.core").respond(200, '');
+      ctrl = $controller(NamespaceCtrl, {$scope: scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("resets vars filter when loaded", function() {
+      expect(scope.resetVarsFilter).toHaveBeenCalled();
+    });
+
+    it("sets the title with the correct namespace", function() {
+      expect(scope.setTitle).toHaveBeenCalledWith(scope.nsName);
+    });
+
+    it("assigns nsName correctly", function() {
+      expect(scope.nsName).toEqual("clojure.core");
+    });
+
+    it("handleNsDoc assigns docstring", function() {
+      scope.handleNsDoc("hello");
+      expect(scope.docstring).toEqual("hello");
+    });
+
+    it("handles indicates the lack of namespace docs", function() {
+      scope.handleNsDoc('');
+      expect(scope.docstring).toEqual("No Namespace Docs.");
+    });
+  });
+
+  describe("Controller: VarInfoCtrl", function() {
+    var $httpBackend;
+
+    beforeEach(inject(function($routeParams, $rootScope, $controller, _$httpBackend_) {
+      $routeParams.namespace = "ns";
+      $routeParams.var = "vr";
+      scope = $rootScope.$new();
+      scope.vars = [];
+      scope.loadVars = function() {};
+      scope.handleResponse = function() {};
+      $httpBackend = _$httpBackend_;
+      $httpBackend.when('GET', /.*/).respond(200, '');
+      ctrl = $controller(VarInfoCtrl, {$scope: scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("#handleVarDoc handles docs correctly", function() {
+      scope.handleVarDoc("hello world");
+      expect(scope.docstring).toBe("hello world");
+    });
+
+    it("#handleVarDoc indicates when docs not found", function() {
+      scope.handleVarDoc("");
+      expect(scope.docstring).toBe("No Docs.");
+    });
+
+    it("#handleSource handles source correctly", function() {
+      scope.handleSource("(some source)");
+      expect(scope.source).toBe("(some source)");
+    });
+
+    it("#handleSource indicates source not found", function() {
+      scope.handleSource(null);
+      expect(scope.source).toBe("Source not found.");
+    });
+
+    it("#refreshVars should reload vars when varlist is empty", function() {
+      scope.vars = [];
+      spyOn(scope, "loadVars");
+      scope.refreshVars();
+      expect(scope.loadVars).toHaveBeenCalled();
+    });
+
+    it("#refreshVars should not reload vars if varlist exists", function() {
+      scope.vars = ["one", "two", "three"];
+      spyOn(scope, "loadVars");
+      scope.refreshVars();
+      expect(scope.loadVars).not.toHaveBeenCalled();
     });
   });
 });
