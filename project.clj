2514fba0b5d5ea9f066a63c5af3a05f5f9dd4367
Haim Ashkenazi <haim@babysnakes.org>
2013-06-10 00:09:00 +0300

    Bringing tools/dependencies up to date!
    Updated clojure, npm and asset dependencies. Some notes:
    
    - Update required leiningen to 2.2.
    - lein-pedantic is partly incorporated into lein 2.2.
    - grunt-contrib-watch updates offers some new
      functionality (livereload). Need to learn about that.
    - Currently node version higher then 0.10.7 break karma with phantomjs.

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.nrepl "0.2.2"]
+                 [org.clojure/tools.nrepl "0.2.3"]
                  [ring/ring-core "1.1.8"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [compojure "1.1.5"
                   :exclusions [org.clojure/tools.macro]]
-                 [com.taoensso/timbre "1.5.2"]
+                 [com.taoensso/timbre "2.1.2"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-json "0.2.0"]
                  [ring-anti-forgery "0.3.0-SNAPSHOT"]]
@@ -18,11 +18,9 @@
   :test-paths ["test/clojure"]
   
   :profiles {:dev
-             {:dependencies [[ring-mock "0.1.3"]]
-              :plugins [[lein-pedantic "0.0.5"]]
+             {:dependencies [[ring-mock "0.1.5"]]
               :jvm-opts ["-Dayler.dev=t"]}}
 
   :main ayler.app
 
-  :pedantic :warn
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.2.0")
