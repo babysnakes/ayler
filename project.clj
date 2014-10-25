aaecc9b42283f67b0f1c5044c85c7191d5b9fc21
Haim Ashkenazi <haim@babysnakes.org>
2014-10-25 20:23:23 +0300

    Version 0.4.3!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ayler "0.4.3-SHAPSHOT"
+(defproject ayler "0.4.3"
   :description "External namespace browswer for clojure projects."
   ;; :url "http://example.com/FIXME"
   :license {:name "GPLv3"
